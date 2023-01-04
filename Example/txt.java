eba0aef1ada2fb9801a821c9a715b13136b7dd77
dhanashriregoti <121925048+dhanashriregoti@users.noreply.github.com>
2023-01-04 13:24:09 +0530

    3rd commit

---
+++ Example/txt.java
@@ -0,0 +1,10 @@
+EduHorizon & attendanceGuru running/ live websites overview
+Website
+Apps (Android /IOS)
+Set up the environment and workspace
+Note : install ubuntu os , java- openjdk 11, maven,  intellij, curl, tomcat 
+Create a bitbucket account // 
+Install Postman application  
+Try to run some request and see the responses
+Try to learn the methods objective 
+(get, post, patch , put , delete)
